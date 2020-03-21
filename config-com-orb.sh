82164df491cfb78e1ea8c9968cb11cf7d49877c8
Jianglong Liao <mike.liaojl@gmail.com>
2020-03-21 15:12:52 +0800

    Final experiment results

--- config-com-orb.sh
+++
@@ -1,5 +0,0 @@
-for i in 0.00001 0.00005 0.0001 0.00015 0.0005 0.00125 0.0015 0.00175
-do
-    ./build/bin/benchmark_loader -i datasets/ICL_NUIM/living_room_traj2_loop.slam -load ./build/lib/liborbslam2-original-library.so -filter ./build/lib/libcombine-skip-flow-filter.so -fth $i -o ./experiments/final/config/ICL2-ORB-COM-$i.log
-    ./build/bin/benchmark_loader -i datasets/ICL_NUIM/living_room_traj3_loop.slam -load ./build/lib/liborbslam2-original-library.so -filter ./build/lib/libcombine-skip-flow-filter.so -fth $i -o ./experiments/final/config/ICL3-ORB-COM-$i.log
-done
